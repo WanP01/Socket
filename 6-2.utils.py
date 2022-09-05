27b5e36b4165c535da34b233dc565dd87c57e466
WanP01 <wanpwc@163.com>
2022-09-05 23:25:58 +0800

    最新提交

--- 6-2.utils.py
+++
@@ -1,8 +0,0 @@
-#拼接响应报文
-def create_request_http(status,request_body):
-    request_line = 'HTTP/1.1 %s\r\n'%status
-    request_header = '\r\n'
-    request_blank = '\r\n'
-    request_data = request_line + request_header + request_blank + request_body
-
-    return request_data
